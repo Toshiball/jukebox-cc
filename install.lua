e0d17a23c18ec8fcb580f2735b91f1d9534fbb72
Toshiball <64008713+Toshiball@users.noreply.github.com>
2024-06-19 21:28:11 +0200

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,23 @@
+local baseUri = ""
+local files = { "help", "play", "save", "savetodevice", "startup", "menu", "setvolume" }
+
+term.clear()
+
+for _, file in pairs(files) do
+	print("Downloading program '" .. file .. "'...")
+
+	local fileInstance = fs.open(file .. ".lua", "w")
+	local response = http.get(baseUri .. file .. ".lua")
+
+	fileInstance.write(response.readAll())
+	fileInstance.close()
+end
+
+local updateUri = "/version.txt"
+
+local updateResponse = http.get(updateUri)
+local updateFile = fs.open("version.txt", "w")
+
+updateFile.write(updateResponse.readAll())
+
+print("Installation complete! Please restart your computer.")
